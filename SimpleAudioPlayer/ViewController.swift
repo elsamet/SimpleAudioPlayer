f884ce82dc773b7ebab26fc5fbd124b16422fc75
elsamet <eng.mahmoudramadan2012@gmail.com>
2017-04-14 18:33:43 +0200

    add start, pause and replay options for audio

--- SimpleAudioPlayer/ViewController.swift
+++ SimpleAudioPlayer/ViewController.swift
@@ -7,10 +7,50 @@
 //
 
 import UIKit
+import  AVFoundation
 
 class ViewController: UIViewController {
-
- 
-
+    
+    var audioplayer:AVAudioPlayer = AVAudioPlayer()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        do {
+            
+            let audioPath = Bundle.main.path(forResource: "087", ofType: "mp3")
+            let url = URL(fileURLWithPath: audioPath!)
+            
+            try audioplayer = AVAudioPlayer(contentsOf: url)
+            
+            
+        } catch  {
+            print("error")
+        }
+    }
+    
+    
+    
+    @IBAction func playAudio(_ sender: Any) {
+        audioplayer.play()
+    }
+    
+    
+    
+    @IBAction func pauseAudio(_ sender: Any) {
+        audioplayer.pause()
+    }
+    
+    
+    
+    
+    @IBAction func replayAudio(_ sender: Any) {
+        audioplayer.currentTime=0
+        
+    }
+    
+    
+    
+    
 }
 
